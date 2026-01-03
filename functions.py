fca7db0dd816964b201a0847521ba2992bdd14c1
AnikaAnmol <anmolsaras@gmail.com>
2026-01-03 19:39:46 +0600

    Commited

--- functions.py
+++ functions.py
@@ -2,10 +2,12 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 7
+    return a - b  # <--- fix this in step 7
 
 def multiply(a, b):
     return a * b
 
-def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+def convert_fahrenheit_to_celsius(f):
+    # Absolute zero in Fahrenheit is -459.67
+    assert f >= -459.67, "Temperature below absolute zero"
+    return (f - 32) * 5 / 9 # <-- Fix this in step 7
